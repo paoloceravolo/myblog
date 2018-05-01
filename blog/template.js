437da8db7d5d8aafa7ac6451c3f72f469cc62893
Paolo Ceravolo <paoloceravolo@Paolos-MacBook-Pro.local>
2018-05-01 10:07:06 +0200

    template caricato con import

--- blog/template.js
+++ blog/template.js
@@ -1,4 +1,18 @@
 export function template(){
-	var view = "<div class='posts'><p><%= denominazione_museo %></p></div>"
+	var view = "\
+<section class='post'>\
+    <header class='post-header'>\
+        <h2 class='post-title'><%= denominazione_museo %></h2>\
+                        <p class='post-meta'>\
+                            By <a class='post-author' href='<% if(typeof(sito_web_sede) !== 'undefined'){%> <%= sito_web_sede %> <% } %>'><%= comune_sede %></a>\
+                        </p>\
+                    </header>\
+                    <div class='post-description'>\
+                        <p>\
+                            <%= denominazione_sede %>\
+                        </p>\
+                    </div>\
+                </section>\
+";
 	return view;
 }
