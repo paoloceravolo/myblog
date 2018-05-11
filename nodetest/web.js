47499309fe7ecb6479f12de9284c0be925ae56dd
Paolo Ceravolo <paoloceravolo@cln-244-32.eduroam.unimi.it>
2018-05-11 10:42:06 +0200

    primoserver

--- nodetest/web.js
+++ nodetest/web.js
@@ -1,12 +1,28 @@
 const http = require('http');
+const fs = require('fs');
 
-function handle(req,res){
+var s = http.createServer();
+// s.on ('request', (req, res) => {});
+s.on('request', function(req,res){
+	
 	console.log('Mi hai chiamato con metodo: '
 		+ req.method + 'e con URL: '
 		+ req.url);
-res.writeHead(200,{'Content-Type': 'application/json'});
-res.end(JSON.stringify({error: null})+"\n");
-}
 
-var s = http.createServer(handle);
+	var appRoot = process.cwd();
+	var path = appRoot + req.url;
+
+	fs.readdir(path, function(err,files){
+		//console.log(files);
+		if(err){
+res.writeHead(400,{'Content-Type': 'application/json'});
+res.end(JSON.stringify({error: err})+"\n");
+		};
+res.writeHead(200,{'Content-Type': 'application/json'});
+res.end(JSON.stringify({data: files})+"\n");
+
+	});
+
+
+});
 s.listen(8383);
