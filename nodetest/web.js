82a303eff31727b45a50e7af177c50652f296310
Paolo Ceravolo <paoloceravolo@cln-244-32.eduroam.unimi.it>
2018-05-11 11:22:00 +0200

    primoserver

--- nodetest/web.js
+++ nodetest/web.js
@@ -1,6 +1,14 @@
 const http = require('http');
 const fs = require('fs');
 
+function lista_dir(path,callback){
+	fs.readdir(path,function(err,files){
+		if(err){callback(err);return}
+
+		callback(null,files);
+	});
+};
+
 var s = http.createServer();
 // s.on ('request', (req, res) => {});
 s.on('request', function(req,res){
@@ -12,17 +20,16 @@ s.on('request', function(req,res){
 	var appRoot = process.cwd();
 	var path = appRoot + req.url;
 
-	fs.readdir(path, function(err,files){
-		//console.log(files);
+	lista_dir(path,function(err, listed){
+		var error = {'error': err};
+		var output = {'error': null,
+		'data': listed};
 		if(err){
 res.writeHead(400,{'Content-Type': 'application/json'});
-res.end(JSON.stringify({error: err})+"\n");
-		};
+res.end(JSON.stringify(error)+"\n");
+		}
 res.writeHead(200,{'Content-Type': 'application/json'});
-res.end(JSON.stringify({data: files})+"\n");
-
+res.end(JSON.stringify(output)+"\n");
 	});
-
-
 });
 s.listen(8383);
