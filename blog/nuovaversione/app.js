1dfaf21d2f0c4a50feeb4394dec872d52e2d284c
Paolo Ceravolo <paoloceravolo@Paolos-MacBook-Pro.local>
2018-05-18 09:59:10 +0200

    express

--- blog/nuovaversione/app.js
+++ blog/nuovaversione/app.js
@@ -2,18 +2,26 @@ const express = require('express');
 const app = express();
 const port = 8383;
 
-app.get('/', function(){
+app.get('/', function(req, res){
 	console.log('Sono qui mi hai chiamato!');
+	res.send('Sono qui mi hai chiamato!\n');
 });
 
-app.get('/musei/', function(){
+app.get('/musei/:id_museo', function(req, res){
 	console.log('se cerchi musei sei nel posto giusto');
+	var museo = req.params.id_museo;
+	res.send('Questa è la pagina del museo '+museo+'\n');
 });
 
 app.get('/mappa/', function(){
-	console.log('se cerchi musei sei nel posto giusto');
+	console.log('se cerchi la mappa sei nel posto giusto');
 });
 
 app.listen(port, function(){
 	console.log('Eseguo sulla porta '+port);
+});
+
+app.use(function(err, req, res, next){
+	conole.log(err.stack);
+	res.status(500).send('Qualcosa non va! \n');
 });
